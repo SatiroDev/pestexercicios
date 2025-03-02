612dfe3f19e19fbc3d2b3a7b636db936dbc7d0af
Satiro07 <jardelsatiro57@gmail.com>
2025-03-01 21:37:14 -0300

    att

--- exer6.py
+++ exer6.py
@@ -1,4 +1,4 @@
 base = int(input('base do triângulo: '))
 altura = int(input('altura do triângulo: '))
 area = (base * altura) / 2
-print(f'A área do triângulo é {area}')
+print(f'A área do triângulo é {area:.2f}')
