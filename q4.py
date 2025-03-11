b580c937d57e9747cd1356f551563319483061ba
Satiro07 <jardelsatiro57@gmail.com>
2025-03-11 20:56:00 -0300

    Update q4.py

--- q4.py
+++ q4.py
@@ -1,11 +1,9 @@
 soma = 0
 num1 = int(input('Digite um número: '))
-soma += num1
 num2 = int(input('Digite um número: '))
-soma += num2
 num3 = int(input('Digite um número: '))
-soma += num3
 if num1 != num2 and num2 != num3 and num1 != num3:
+    soma = num1 + num2 + num3
     print(f'A soma dos números foi de {soma}')
 else:
     print('Soma = 0')
