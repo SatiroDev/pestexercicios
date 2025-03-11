b479736f053863ccb0175adb6bb5bb735a63d9dd
Satiro07 <jardelsatiro57@gmail.com>
2025-03-11 20:31:31 -0300

    Update q6.py

--- q6.py
+++ q6.py
@@ -1,4 +1,4 @@
-num = 55550
+num = int(input('Digite um número com 5 digitos: '))
 n = 0
 nume = num
 soma = 0
@@ -15,61 +15,16 @@ num4 = num % 10
 num = num // 10
 
 num5 = num
-n1 = 0
-if num1 == num2:
-    n += 3
-    n1 += 1
-    soma += num1
-else:
-    if num1 == num3:
-        n += 3
-        n1 += 1
-        soma += num1
-    else:
-        if num1 == num4:
-            n += 3
-            n1 += 1
-            soma += num1
-        else:
-            if num1 == num5:
-                n += 3
-                n1 += 1
-                soma += num1
 
-if num2 == num3:
-    n += 3
-    n1 += 1
-    soma += num2
-else:
-    if num2 == num4:
-        n += 3
-        n1 += 1
-        soma += num2
-    else:
-        if num2 == num5:
-            n += 3
-            n1 += 1
-            soma += num2
 
-if num3 == num4:
-    n += 3
-    n1 += 1
-    soma += num3
 
-else:
-    if num3 == num5:
-        n += 3
-        n1 += 1
-        soma += num3
-
-if num4 == num5:
-    n += 3
-    n1 += 1
-    soma += num4
-numeri = soma / n1
-
-print(numeri)
-if numeri >= 2:
+if num1 == num2 or num1 == num3 or num1 == num4 or num1 == num5:
+    print('um digito se repete mais de 2 vezes')
+elif num2 == num3 or num2 == num4 or num2 == num5:
+    print('um digito se repete mais de 2 vezes')
+elif num3 == num4 or num3 == num5:
+    print('um digito se repete mais de 2 vezes')
+elif num4 == num5:
     print('um digito se repete mais de 2 vezes')
 else:
     print('um digito não se repete mais de 2 vezes')
