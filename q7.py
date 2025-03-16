e88b445d04ed4b7259b4da52cbcb6b3ac795211f
Satiro07 <jardelsatiro57@gmail.com>
2025-03-16 19:28:13 -0300

    Update q7.py

--- q7.py
+++ q7.py
@@ -1,14 +1,11 @@
 num = 110
 nume = num
-
-
 inv = 0
 inv = inv * 10 + num % 10
 num //= 10
 inv = inv * 10 + num % 10
 num //= 10
 inv = inv * 10 + num % 10
-
 if inv == nume:
     print(f'O número {nume} é palíndromo!')
 else:
