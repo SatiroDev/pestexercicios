fa7cd6ff7159179587b0a768824fc5ecad0836d5
Satiro07 <jardelsatiro57@gmail.com>
2025-03-18 19:53:15 -0300

    .

--- q1.py
+++ q1.py
@@ -9,22 +9,6 @@ elif num % 2 == 0:
 else:
     impares = num
 
-
 print(pares)
 print(impares)
 
-# numero = int(input("Digite um número: "))
-
-# pares = "não é par"
-# impares = "não é ímpar"
-
-# if (numero == 0):
-#     print("Zero é neutro")
-# elif (numero % 2 == 0):
-#     pares = numero
-# else:
-#     impares = numero
-
-
-# print(pares)
-# print(impares)
