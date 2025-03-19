b2072923d2cd763a676e8e8d78d32c45295720cf
Satiro07 <jardelsatiro57@gmail.com>
2025-03-19 19:08:19 -0300

    Update qu8.py

--- qu8.py
+++ qu8.py
@@ -1,7 +1,8 @@
 num_binario = int(input('Digite um número binário: '))
 nume = num_binario
+copia_bin = num_binario
 quant_digs = -1
-soma = 0
+decimal = 0
 while nume:
     nume //= 10
     quant_digs += 1
@@ -9,6 +10,6 @@ cont = 0
 while cont <= quant_digs:
     mult = (num_binario % 10 * (2**cont))
     num_binario //= 10
-    soma += mult
+    decimal += mult
     cont += 1
-print(soma)
+print(f'{copia_bin} binarios corresponde ao número decimal -> {decimal}')
