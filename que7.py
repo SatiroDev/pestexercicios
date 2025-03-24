a91e181c02e2bad95e1a15377efc2dd455dcfa57
Satiro07 <jardelsatiro57@gmail.com>
2025-03-24 20:26:44 -0300

    Update que7.py

--- que7.py
+++ que7.py
@@ -5,6 +5,5 @@ for i in range(1, 101):
         if i % cont == 0:
             c += 1
     if c == 2:
-        print(i)
         soma += i
-print(f'A soma foi de {soma}')
+print(f'A soma dos números primos de 1 até 100 foi de {soma}')
