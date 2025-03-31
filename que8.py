c94e1626003dafa7c55af8f9d7c52bab2374df74
Satiro07 <jardelsatiro57@gmail.com>
2025-03-31 19:10:04 -0300

    Update que8.py

--- que8.py
+++ que8.py
@@ -1,6 +1,6 @@
 n = int(input('Digite um número: '))
-for i in range(0, n+1):
-    fatorial = 0
+for i in range(1, n+1):
+    fatorial = 1
     for c in range(i, 0, -1):
         fatorial *= c
     print(f'{i}! = {fatorial}')
