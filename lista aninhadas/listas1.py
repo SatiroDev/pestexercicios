0f129e9885d443b62e6afa07f69e4e6032c2addc
Satiro07 <jardelsatiro57@gmail.com>
2025-05-24 10:16:31 -0300

    Create listas1.py

---
+++ lista aninhadas/listas1.py
@@ -0,0 +1,17 @@
+# Crie um programa que peça ao usuário para especificar o número de linhas e colunas de uma matriz. Em seguida, preencha a matriz com números inteiros fornecidos pelo usuário. Use listas aninhadas para representar a matriz.
+
+matriz = []
+
+linhas = int(input('Digite quantas linhas você vai querer na matriz: '))
+
+colunas = int(input('Digite quantas linhas você vai querer na matriz: '))
+
+for i in range(linhas):
+    linha = []
+    for y in range(colunas):
+        elemento = int(input(f'Digite um número para posição ({i}, {y}): '))
+        linha.append(elemento)
+    matriz.append(linha)
+
+for linhas in matriz:
+    print(linhas)
